c9e9c8c5045d21deb44ca19d58709b8603caec8c
Yuki Harada <yuki.freelife@gmail.com>
2025-07-08 19:27:35 +0900

    404エラー改善できるか？？

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   // 通常は特に設定不要だが、公開時 base を調整する場合ここに書く
-  base: '/yutabito',
+  base: './',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        list: resolve(__dirname, 'list.html'),
+        favorites: resolve(__dirname, 'favorites.html'),
+        onsen: resolve(__dirname, 'onsen.html'),
+      }
+    }
+  }
 });
