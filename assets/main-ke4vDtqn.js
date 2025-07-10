fa19a40fdcc02ad2b1be705e7478f8ada4b1abf9
Yuki Harada <yuki.freelife@gmail.com>
2025-07-10 15:04:50 +0900

    Updates

---
+++ assets/main-ke4vDtqn.js
@@ -0,0 +1,7 @@
+import{s as i}from"./favorite-DccFiVyW.js";function l(e){return`/yutabito/assets/images/onsen_${e}.jpg`}function c(e){const n=document.getElementById("recommended-slider");n.innerHTML="",[...e].sort(()=>.5-Math.random()).slice(0,5).forEach(t=>{const s=document.createElement("div");s.className="slider-card",s.innerHTML=`
+      <a href = "onsen.html?id=${t.id}">
+        <img src="${l(t.romaji)}" alt="${t.name}">
+        <h4>${t.name}</h4>
+      </a> 
+      <button class="favorite-btn" data-id="${t.id}">♡</button>
+    `,n.appendChild(s)}),i()}let d=[];fetch("./onsen.json").then(e=>e.json()).then(e=>{d=e,c(e)}).catch(e=>{console.error("温泉データの取得に失敗しました:",e)});const r=document.getElementById("showFavoritesBtn");r&&r.addEventListener("click",()=>{const e=JSON.parse(localStorage.getItem("favorites")||"[]"),n=new Set(e.map(String)),a=d.filter(o=>n.has(String(o.id)));c(a),i()});
